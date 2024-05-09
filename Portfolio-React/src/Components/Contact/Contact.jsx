4ebc4bd17889d4d20ea15f1f0c3f515b062f0ada
Harshad kumbhar <harshadkumbhar0822@outlook.com>
2024-05-09 20:08:55 +0530

    6

--- Portfolio-React/src/Components/Contact/Contact.jsx
+++ Portfolio-React/src/Components/Contact/Contact.jsx
@@ -5,6 +5,28 @@ import mail_icon from '../../assets/mail_icon.svg'
 import location_icon from '../../assets/location_icon.svg'
 import call_icon from '../../assets/call_icon.svg'
 const Contact = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "95636333-039e-4de7-9965-c1349fb97995");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      alert(res.message)
+    }
+  };
   return (
     <div id='contact' className='contact'>
         <div className="contact-title">
@@ -32,7 +54,7 @@ const Contact = () => {
                     </div>
              </div>
                 <div>
-                  <form className='contact-right'>
+                  <form onSubmit={onSubmit} className='contact-right'>
                   <label htmlFor="">Your Name</label>
                   <input type="text" placeholder='Enter your name' name='name' />
                   <label htmlFor="">Your Email</label>
--- Portfolio-React/src/Components/Hero/Hero.jsx
+++ Portfolio-React/src/Components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 const Hero = () => {
   return (
     <div id='hero' className='hero'>
@@ -8,7 +9,7 @@ const Hero = () => {
       <h1><span>I,m Harshad Kumbhar,</span>Fullstack Blockchain Developer</h1>
       <p>I am MERN Stack Developer from Maharashtra, INDIA with MCA degree as Fresher</p>
       <div className="hero-action">
-        <div className="hero-connect">Connect with Me</div>
+        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with Me</AnchorLink></div>
         <div className="hero-resume">My Resume</div>
       </div>
     </div>
