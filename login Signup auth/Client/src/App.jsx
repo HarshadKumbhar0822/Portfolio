86429b296df18a55b32b1d04f9f461f643bf12b8
Harshad kumbhar <harshadkumbhar0822@outlook.com>
2024-05-12 00:17:45 +0530

    1

---
+++ login Signup auth/Client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+  <>
+    <Navbar />
+    <Route path='/' element={<Home />} />
+  </>
+  )
+}
+
+export default App
